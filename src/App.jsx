c5a7b7fc15968fa8e9fc2fa55862f19398f4d0fd
Nicole Dai <dai.nicole@wustl.edu>
2025-04-01 17:36:26 -0500

    changes players for depending on nextSquare

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-
-
 import { useState } from 'react';
+
 function Square({ value, onSquareClick }) {
   return (
     <button className="square" onClick={onSquareClick}>
@@ -10,15 +9,21 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
-    return (
+  return (
     <>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
@@ -37,4 +42,4 @@ export default function Board() {
       </div>
     </>
   );
-};
+}
